92c697df4a60e7a02d3fc693d33d3f891a0e14a7
Asep Nur Ilyas <asepnurilyas09@gmail.com>
2026-06-29 12:00:23 +0700

    Delete y directory

--- y/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
-
-// Mendaftarkan endpoint login admin
-Route::post('/login', [AuthController::class, 'login']);
